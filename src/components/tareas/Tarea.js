d6ac636a08c26e40b80d15a621869e9dc62d5083
Nar <dev@webdevelop.dev>
2021-04-08 18:57:10 +0200

    Terminamos el proyecto FrontEnd

--- src/components/tareas/Tarea.js
+++ src/components/tareas/Tarea.js
@@ -1,7 +1,43 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 const Tarea = ({ tarea }) => {
+
+
+     //Extraer si un proyecto esta activo
+     const proyectosContext = useContext(proyectoContext);
+     const { proyecto } = proyectosContext;
+
+     //Obtener la funcion del context de tarea
+     const tareasContext = useContext(tareaContext);
+     const { eliminarTarea, obtenerTareas, cambiarEstadoTarea, guardarTareaActual } = tareasContext;
+
+     //Extraer el proyecto actual
+     const [proyectoActual] = proyecto;
+
+     //Funcion que se ejecuta cuando el usuario presiona el btn de eliminar tarea
+     const tareaEliminar = id => {
+          eliminarTarea(id);
+          obtenerTareas(proyectoActual.id);
+     }
+
+     //Funcion que modifica el estado de las tareas
+     const cambiarEstado = tarea => {
+          if (tarea.estado) {
+               tarea.estado = false;
+          } else {
+               tarea.estado = true;
+          }
+
+          cambiarEstadoTarea(tarea);
+     }
+
+     //Agrega una tarea actual cuando el usuario quiera editarla
+     const seleccionarTarea = tarea => {
+          guardarTareaActual(tarea);
+     }
+
      return (
           <li className="tarea sombra">
                <p>{tarea.nombre}</p>
@@ -11,6 +47,7 @@ const Tarea = ({ tarea }) => {
                               <button
                                    type="button"
                                    className="completo"
+                                   onClick={() => cambiarEstado(tarea)}
                               >
                                    Completo
                               </button>
@@ -18,6 +55,7 @@ const Tarea = ({ tarea }) => {
                               <button
                                    type="button"
                                    className="incompleto"
+                                   onClick={() => cambiarEstado(tarea)}
                               >
                                    incompleto
                               </button>
@@ -28,11 +66,13 @@ const Tarea = ({ tarea }) => {
                     <button
                          type="button"
                          className="btn btn-primario"
+                         onClick={() => seleccionarTarea(tarea)}
                     >Editar
                     </button>
                     <button
                          type="button"
                          className="btn btn-secundario"
+                         onClick={() => tareaEliminar(tarea.id)}
                     >Eliminar</button>
                </div>
           </li>
